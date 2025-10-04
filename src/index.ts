ad1f5d3d931f5cfbdf13d0d43726d081e8db2b0f
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-04 09:54:56 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello, World!');
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+export const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: Author
+  }
+  type Author {
+    name: String
+    books: [Book]
+  }
+  type Query {
+    books: [Book]
+  }
+`;
+
+export const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
