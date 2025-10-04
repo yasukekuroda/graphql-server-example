7de4f4f5e8c4e77a8fa978df7dbbaa0d0dc248f5
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-04 11:30:55 +0900

    a

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -1,13 +1,48 @@
 import { ApolloServer } from "@apollo/server";
 
+const libraries = [{ branch: "main" }, { branch: "staging" }];
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+    branch: "main",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+    branch: "staging",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+    branch: "staging",
+  },
+];
+
 export const typeDefs = `#graphql
+  type Library {
+    branch: String!
+    books: [Book]
+  }
+  type Book {
+    title: String!
+    author: String!
+    branch: String!
+  }
   type Query {
-    books: [String]
+    libraries: [Library]
+    books: [Book]
   }
 `;
 
 export const resolvers = {
   Query: {
-    books: () => ["Book 1", "Book 2", "Book 3"],
+    libraries: () => libraries,
+    books: () => books,
+  },
+  Library: {
+    books: (parent: { branch: string }) => {
+      return books.filter((book) => book.branch === parent.branch);
+    },
   },
 };
