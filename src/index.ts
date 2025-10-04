783f1fad384b1208828c31725ffa19abafd530f6
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-04 10:28:22 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { ApolloServer } from "@apollo/server";
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 export const typeDefs = `#graphql
+  enum AllowedColor {
+    RED
+    GREEN
+    BLUE
+  }
+  input AddBookInput {
+    title: String
+    author: String
+  }
   type Book {
     title: String
     author: Author
@@ -13,9 +23,11 @@ export const typeDefs = `#graphql
   type Query {
     books: [Book]
     authors: [Author]
+    favoriteColor: AllowedColor
+    avatar(borderColor: AllowedColor): String
   }
   type Mutation {
-    addBook(title: String, author: String): Book
+    addBook(input: AddBookInput): Book
   }
 `;
 
@@ -25,13 +37,21 @@ export const resolvers = {
     authors: () => {
       return books.map((book) => book.author);
     },
+    favoriteColor: () => "BLUE",
+    avatar: (_: undefined, args: { borderColor: string }) => {
+      return "this is sample avatar url";
+    },
   },
   Mutation: {
-    addBook: (_: undefined, args: { title: string; author: string }) => {
+    addBook: (
+      _: undefined,
+      args: { input: { title: string; author: string } },
+    ) => {
+      const { title, author } = args.input;
       const newBook = {
-        title: args.title,
+        title,
         author: {
-          name: args.author,
+          name: author,
           books: [],
         },
       };
