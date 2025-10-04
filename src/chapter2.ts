6785bdf31f10d50f8bceb5ca8bf4be6accc26e30
yasuke.kuroda <yasuke.kuroda@gmail.com>
2025-10-04 11:17:46 +0900

    a

--- src/chapter2.ts
+++ src/chapter2.ts
@@ -4,12 +4,10 @@ export const typeDefs = `#graphql
   type Query {
     books: [String]
   }
-  type Mutation {}
 `;
 
 export const resolvers = {
   Query: {
     books: () => ["Book 1", "Book 2", "Book 3"],
   },
-  Mutation: {},
 };
